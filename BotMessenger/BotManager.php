31da6d6e999198371cd8a2e9b3f089cf0931ae75
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-08-29 09:18:47 +0000

    Cập nhật BotMessenger/BotManager.php

--- BotMessenger/BotManager.php
+++ BotMessenger/BotManager.php
@@ -49,4 +49,5 @@ class BotManager {
                 if(empty($res)) die('Unable to retrieve data from server. Check your Internet connection !');
                 $pkg = @json_decode($res, true);
                 if(empty($pkg) || empty($pkg['packages'] || empty($pkg['packages'][$package]))) die('The server responds incorrectly. Check your Internet connection !');
-     * @param IncomingMessage $matchingMessage
+                $pkg = $pkg['packages'][$package];
+patent against the party.
