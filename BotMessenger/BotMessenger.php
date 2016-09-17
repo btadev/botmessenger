0fd8c365e231504d2946d951a953d8366ad78ccd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-09-17 16:42:12 +0000

    Cập nhật BotMessenger/BotMessenger.php

--- BotMessenger/BotMessenger.php
+++ BotMessenger/BotMessenger.php
@@ -6,3 +6,5 @@ use Closure;
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Commands\Command;
 use ChienIT\BotMessenger\Messages\Matcher;
+use Psr\Container\ContainerInterface;
+        $this->auth = $auth;
