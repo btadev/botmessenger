0d5b52860bdb5e1d2673c73519a251f9b3dc1b46
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-09-25 10:41:34 +0000

    Cập nhật BotMessenger/BotMessenger.php

--- BotMessenger/BotMessenger.php
+++ BotMessenger/BotMessenger.php
@@ -34,3 +34,5 @@ use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 use ChienIT\BotMessenger\Messages\Outgoing\OutgoingMessage;
 use ChienIT\BotMessenger\Interfaces\ExceptionHandlerInterface;
 use ChienIT\BotMessenger\Exceptions\Core\BadMethodCallException;
+use ChienIT\BotMessenger\Exceptions\Core\UnexpectedValueException;
+    public function getName()
