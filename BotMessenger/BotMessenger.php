169229f968e6b07def3cb5127986a5e97aafe80e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-09-25 18:18:01 +0000

    Cập nhật BotMessenger/BotMessenger.php

--- BotMessenger/BotMessenger.php
+++ BotMessenger/BotMessenger.php
@@ -235,4 +235,4 @@ class BotMessenger
 
         $user = $this->getDriver()->getUser($this->getMessage());
         $this->cache->put('user_'.$this->driver->getName().'_'.$user->getId(), $user, $this->config['user_cache_time'] ?? 30);
-    protected $driver = '';
+
