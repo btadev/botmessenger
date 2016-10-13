89b91ac5d469c5c37f8ba8ab63a5301c840140b7
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-10-13 15:18:35 +0000

    Cập nhật BotMessenger/BotMessenger.php

--- BotMessenger/BotMessenger.php
+++ BotMessenger/BotMessenger.php
@@ -406,4 +406,5 @@ class BotMessenger
                      */
                     if (method_exists($this->getDriver(), 'messagesHandled')) {
                         $this->getDriver()->messagesHandled();
-    /**
+                    }
+    public function getText()
