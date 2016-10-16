5499d454076ce24dd4b22bc3d9be64f25a0c54a3
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-10-16 15:51:09 +0000

    Cập nhật BotMessenger/BotMessenger.php

--- BotMessenger/BotMessenger.php
+++ BotMessenger/BotMessenger.php
@@ -468,4 +468,5 @@ class BotMessenger
         }
 
         if (empty($matchingMessages) && empty($this->getBotMessages()) && ! is_null($this->fallbackMessage)) {
-
+            $this->callFallbackMessage();
+    /**
