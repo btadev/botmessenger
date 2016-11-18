d88a8a49aa291e26c8840ac22719695da2e9ed01
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-11-18 17:09:12 +0000

    Cập nhật BotMessenger/BotMessengerFactory.php

--- BotMessenger/BotMessengerFactory.php
+++ BotMessenger/BotMessengerFactory.php
@@ -45,4 +45,5 @@ class BotMessengerFactory
      * @param array $config
      * @param CacheInterface $cache
      * @param Request $request
-use ChienIT\BotMessenger\Cache\ArrayCache;
+     * @param StorageInterface $storageDriver
+# [![ChienIT](https://s.gravatar.com/avatar/36d29d01ee8909a7d386291516a94edb?s=32)](https://github.com/chiendevit) *ChienIt Bot Messenger*
