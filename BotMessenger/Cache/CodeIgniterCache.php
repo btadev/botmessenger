2fcddc43c0a46254ccafe277723eb0e4a14bd969
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-12-11 17:04:12 +0000

    Cập nhật BotMessenger/Cache/CodeIgniterCache.php

--- BotMessenger/Cache/CodeIgniterCache.php
+++ BotMessenger/Cache/CodeIgniterCache.php
@@ -52,4 +52,5 @@ class CodeIgniterCache implements CacheInterface
      * @param  string $key
      * @param  mixed $default
      * @return mixed
-    {
+     */
+        $this->attachment = $attachment;
