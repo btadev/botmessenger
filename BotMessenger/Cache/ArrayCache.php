666bf433ea3bcbe04df79b9a33feaf4abf279b1f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2016-12-23 02:00:02 +0000

    Cập nhật BotMessenger/Cache/ArrayCache.php

--- BotMessenger/Cache/ArrayCache.php
+++ BotMessenger/Cache/ArrayCache.php
@@ -50,4 +50,5 @@ class ArrayCache implements CacheInterface
         if (isset($this->cache[$key])) {
             $cached = $this->cache[$key];
             unset($this->cache[$key]);
-     * @param  string $key
+
+        // Do nothing
