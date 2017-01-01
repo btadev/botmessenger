e4de3f2d4a29724cfab4e9664d380885ff16840a
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-01-01 11:07:49 +0000

    Cập nhật BotMessenger/Cache/RedisCache.php

--- BotMessenger/Cache/RedisCache.php
+++ BotMessenger/Cache/RedisCache.php
@@ -9,4 +9,5 @@ use ChienIT\BotMessenger\Interfaces\CacheInterface;
 /**
  * Redis <http://redis.io> cache backend
  * Requires phpredis native extension <https://github.com/phpredis/phpredis#installation>.
-EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+ */
+
