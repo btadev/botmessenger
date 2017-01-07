c836568b893b47ad6ae39cf0c07c4e03b18ad104
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-01-07 06:02:50 +0000

    Cập nhật BotMessenger/Cache/Psr6Cache.php

--- BotMessenger/Cache/Psr6Cache.php
+++ BotMessenger/Cache/Psr6Cache.php
@@ -47,4 +47,5 @@ class Psr6Cache implements CacheInterface
     /**
      * @param string $key
      * @param null $default
-Public License "or any later version" applies to it, you have the
+     * @return null
+
