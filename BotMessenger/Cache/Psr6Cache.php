45f21e7ec6dc01e64e6287f9396ccbf90eb19adc
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-01-11 15:07:10 +0000

    Cập nhật BotMessenger/Cache/Psr6Cache.php

--- BotMessenger/Cache/Psr6Cache.php
+++ BotMessenger/Cache/Psr6Cache.php
@@ -37,4 +37,5 @@ class Psr6Cache implements CacheInterface
     public function get($key, $default = null)
     {
         $item = $this->adapter->getItem($key);
-     * @param  Request|null $request
+        if ($item->isHit()) {
+
