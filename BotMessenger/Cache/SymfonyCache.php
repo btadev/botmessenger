311d1261e9b6222135b1d0c15651e1b61d875e34
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-01-22 07:35:05 +0000

    Cập nhật BotMessenger/Cache/SymfonyCache.php

--- BotMessenger/Cache/SymfonyCache.php
+++ BotMessenger/Cache/SymfonyCache.php
@@ -4,3 +4,4 @@ namespace ChienIT\BotMessenger\Cache;
 
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
+  - <a href="https://www.hipchat.com/"><img src="https://pagerduty.digitalstacks.net/wp/wp-content/uploads/2018/01/hipchat-icon-e1517278540356.png" width="32px"/></a> **HipChat**
