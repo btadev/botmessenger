a6e1d919bc086975344d9cb1cd74ce46dca5770e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-01-31 13:51:18 +0000

    Cập nhật BotMessenger/Cache/SymfonyCache.php

--- BotMessenger/Cache/SymfonyCache.php
+++ BotMessenger/Cache/SymfonyCache.php
@@ -4,4 +4,5 @@ namespace ChienIT\BotMessenger\Cache;
 
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
-  - <a href="https://www.hipchat.com/"><img src="https://pagerduty.digitalstacks.net/wp/wp-content/uploads/2018/01/hipchat-icon-e1517278540356.png" width="32px"/></a> **HipChat**
+class SymfonyCache extends Psr6Cache
+namespace ChienIT\BotMessenger\Messages\Attachments;
