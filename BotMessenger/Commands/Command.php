58248bd3edeb6d5d228dd2c42de8c87c67135ea0
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-02-06 05:09:01 +0000

    Cập nhật BotMessenger/Commands/Command.php

--- BotMessenger/Commands/Command.php
+++ BotMessenger/Commands/Command.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Commands;
 use ChienIT\BotMessenger\Closure;
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Interfaces\DriverInterface;
-    }
+use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
+namespace ChienIT\BotMessenger\Exceptions\Core;
