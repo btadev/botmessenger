9dcdb36d085232415248cd9adf05eb74f88ab57a
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-02-21 07:55:15 +0000

    Cập nhật BotMessenger/Commands/ConversationManager.php

--- BotMessenger/Commands/ConversationManager.php
+++ BotMessenger/Commands/ConversationManager.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Commands;
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Messages\Matcher;
 use ChienIT\BotMessenger\Messages\Incoming\Answer;
-     * @param \ChienIT\BotMessenger\Messages\Incoming\IncomingMessage $message
+use ChienIT\BotMessenger\Messages\Attachments\File;
+     * @param  IncomingMessage $message
