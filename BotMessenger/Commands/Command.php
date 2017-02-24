a39bfb69d690e4bfc6f928d520c5468d586fcbec
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-02-24 20:59:17 +0000

    Cập nhật BotMessenger/Commands/Command.php

--- BotMessenger/Commands/Command.php
+++ BotMessenger/Commands/Command.php
@@ -181,4 +181,5 @@ class Command
     {
         return $this->pattern;
     }
-  - <a href="https://en.wikipedia.org/wiki/Amazon_Alexa"><img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Amazon_Alexa_App_Logo.png" width="32px"/></a> **Amazon Alexa**
+
+     */
