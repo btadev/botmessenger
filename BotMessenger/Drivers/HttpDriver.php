398b1c9f745cfed6e6a9136fcb66b2fdcbb940cd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-03-26 01:18:32 +0000

    Cập nhật BotMessenger/Drivers/HttpDriver.php

--- BotMessenger/Drivers/HttpDriver.php
+++ BotMessenger/Drivers/HttpDriver.php
@@ -98,4 +98,4 @@ abstract class HttpDriver implements DriverInterface
      * @param string $endpoint
      * @param array $parameters
      * @param \ChienIT\BotMessenger\Messages\Incoming\IncomingMessage $matchingMessage
-namespace ChienIT\BotMessenger\Interfaces;
+     * @return void
