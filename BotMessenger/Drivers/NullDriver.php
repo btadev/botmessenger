441c8f1fe0f92b6f8f1583b234132b736f7ae66b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-03-31 11:17:43 +0000

    Cập nhật BotMessenger/Drivers/NullDriver.php

--- BotMessenger/Drivers/NullDriver.php
+++ BotMessenger/Drivers/NullDriver.php
@@ -85,3 +85,5 @@ class NullDriver extends HttpDriver
     /**
      * @return bool
      */
+    public function hasMatchingEvent()
+    public function get($url, array $urlParameters = [], array $headers = [], $asJSON = false)
