fdde589f1ae4c53a0ea866e2de95c57752fc00aa
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-04-04 18:13:10 +0000

    Cập nhật BotMessenger/Drivers/Tests/FakeDriver.php

--- BotMessenger/Drivers/Tests/FakeDriver.php
+++ BotMessenger/Drivers/Tests/FakeDriver.php
@@ -5,4 +5,4 @@ namespace ChienIT\BotMessenger\Drivers\Tests;
 use ChienIT\BotMessenger\Users\User;
 use ChienIT\BotMessenger\Messages\Incoming\Answer;
 use ChienIT\BotMessenger\Interfaces\DriverInterface;
-    public function delete($key)
+use ChienIT\BotMessenger\Interfaces\VerifiesService;
