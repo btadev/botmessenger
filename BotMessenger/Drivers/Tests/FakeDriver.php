87c1c0fd52a62998036ec551bf0e7e86b91cfbc2
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-04-16 04:25:49 +0000

    Cập nhật BotMessenger/Drivers/Tests/FakeDriver.php

--- BotMessenger/Drivers/Tests/FakeDriver.php
+++ BotMessenger/Drivers/Tests/FakeDriver.php
@@ -9,3 +9,5 @@ use ChienIT\BotMessenger\Interfaces\VerifiesService;
 use ChienIT\BotMessenger\Messages\Outgoing\Question;
 use Symfony\Component\HttpFoundation\Request;
 use ChienIT\BotMessenger\Drivers\Events\GenericEvent;
+use Symfony\Component\HttpFoundation\Response;
+        $this->exceptionHandler->register($exception, $this->getCallable($closure));
