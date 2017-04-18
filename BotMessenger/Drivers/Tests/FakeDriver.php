b1f8d35755c12b4cd8b41e9def5fed8cbba2f4bc
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-04-18 19:54:22 +0000

    Cập nhật BotMessenger/Drivers/Tests/FakeDriver.php

--- BotMessenger/Drivers/Tests/FakeDriver.php
+++ BotMessenger/Drivers/Tests/FakeDriver.php
@@ -6,3 +6,5 @@ use ChienIT\BotMessenger\Users\User;
 use ChienIT\BotMessenger\Messages\Incoming\Answer;
 use ChienIT\BotMessenger\Interfaces\DriverInterface;
 use ChienIT\BotMessenger\Interfaces\VerifiesService;
+use ChienIT\BotMessenger\Messages\Outgoing\Question;
+     *
