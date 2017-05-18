e146ab302b68ab9c939848cdd2d020f10dba7e57
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-05-18 13:14:36 +0000

    Cập nhật BotMessenger/Handlers/ExceptionHandler.php

--- BotMessenger/Handlers/ExceptionHandler.php
+++ BotMessenger/Handlers/ExceptionHandler.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Interfaces\ExceptionHandlerInterface;
 
 class ExceptionHandler implements ExceptionHandlerInterface
-     * @param DriverInterface $driver
+{
+    public function getName()
