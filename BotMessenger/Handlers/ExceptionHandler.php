74e21a7e43be774590536193df696be8c0d80e7f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-05-21 11:58:14 +0000

    Cập nhật BotMessenger/Handlers/ExceptionHandler.php

--- BotMessenger/Handlers/ExceptionHandler.php
+++ BotMessenger/Handlers/ExceptionHandler.php
@@ -4,4 +4,4 @@ namespace ChienIT\BotMessenger\Handlers;
 
 use ChienIT\BotMessenger\BotMessenger;
 use Illuminate\Support\Collection;
-it.  (Additional permissions may be written to require their own
+use ChienIT\BotMessenger\Interfaces\ExceptionHandlerInterface;
