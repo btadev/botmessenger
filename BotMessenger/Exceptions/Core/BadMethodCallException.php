b4f0e733d4ce5e8ba4cc1f0df19fda0f0c68bc17
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-05-26 10:21:53 +0000

    Cập nhật BotMessenger/Exceptions/Core/BadMethodCallException.php

--- BotMessenger/Exceptions/Core/BadMethodCallException.php
+++ BotMessenger/Exceptions/Core/BadMethodCallException.php
@@ -3,4 +3,5 @@
 namespace ChienIT\BotMessenger\Exceptions\Core;
 
 use ChienIT\BotMessenger\Exceptions\Base\BotMessengerException;
-    /**
+
+    public function __construct(Command $command, IncomingMessage $message, array $matches)
