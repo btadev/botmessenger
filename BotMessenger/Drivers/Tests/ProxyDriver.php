daeb41dcd5b7164be8f30ba455088330f348494c
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-05-27 16:35:33 +0000

    Cập nhật BotMessenger/Drivers/Tests/ProxyDriver.php

--- BotMessenger/Drivers/Tests/ProxyDriver.php
+++ BotMessenger/Drivers/Tests/ProxyDriver.php
@@ -68,4 +68,5 @@ final class ProxyDriver implements DriverInterface
     public function getUser(IncomingMessage $matchingMessage)
     {
         return self::instance()->getUser($matchingMessage);
-    /**
+    }
+
