b0f7c435717951e7c96ad703279c68a886ff8394
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-01 05:14:37 +0000

    Cập nhật BotMessenger/Exceptions/Core/BadMethodCallException.php

--- BotMessenger/Exceptions/Core/BadMethodCallException.php
+++ BotMessenger/Exceptions/Core/BadMethodCallException.php
@@ -5,3 +5,5 @@ namespace ChienIT\BotMessenger\Exceptions\Core;
 use ChienIT\BotMessenger\Exceptions\Base\BotMessengerException;
 
 class BadMethodCallException extends BotMessengerException
+{
+    public function getUsername();
