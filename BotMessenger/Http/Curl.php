df43b1f597c30a965c32f73f8b63730fe67162ec
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-16 13:24:24 +0000

    Cập nhật BotMessenger/Http/Curl.php

--- BotMessenger/Http/Curl.php
+++ BotMessenger/Http/Curl.php
@@ -40,4 +40,5 @@ class Curl implements HttpInterface
      */
     public function get($url, array $urlParameters = [], array $headers = [], $asJSON = false)
     {
+        $request = $this->prepareRequest($url, $urlParameters, $headers);
      */
