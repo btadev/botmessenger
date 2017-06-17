1ff0e965292747d88b9f2850d1e17b61ee1780b4
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-17 01:49:22 +0000

    Cập nhật BotMessenger/Interfaces/DriverInterface.php

--- BotMessenger/Interfaces/DriverInterface.php
+++ BotMessenger/Interfaces/DriverInterface.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Interfaces;
 use Symfony\Component\HttpFoundation\Response;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
-    alternative is allowed only occasionally and noncommercially, and
+interface DriverInterface
+    }
