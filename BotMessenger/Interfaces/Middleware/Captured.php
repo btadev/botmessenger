f7a011ea2294b687a8c036d5a471a7abbcb9fde2
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-24 16:40:35 +0000

    Cập nhật BotMessenger/Interfaces/Middleware/Captured.php

--- BotMessenger/Interfaces/Middleware/Captured.php
+++ BotMessenger/Interfaces/Middleware/Captured.php
@@ -3,4 +3,5 @@
 namespace ChienIT\BotMessenger\Interfaces\Middleware;
 
 use ChienIT\BotMessenger\BotMessenger;
-            if ($driver->getName() === $name) {
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
+
