1de6a5a62024ae6748bdecc51916dd1cff2275a3
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-06-24 16:54:23 +0000

    Cập nhật BotMessenger/Interfaces/Middleware/Captured.php

--- BotMessenger/Interfaces/Middleware/Captured.php
+++ BotMessenger/Interfaces/Middleware/Captured.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Interfaces\Middleware;
 use ChienIT\BotMessenger\BotMessenger;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
-     * @return array
+interface Captured
+     * @param IncomingMessage $message
