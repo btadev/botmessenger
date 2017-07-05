a58e50d69ff204105901f05eb8fb170ab8e84820
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-05 18:25:08 +0000

    Cập nhật BotMessenger/Interfaces/HttpInterface.php

--- BotMessenger/Interfaces/HttpInterface.php
+++ BotMessenger/Interfaces/HttpInterface.php
@@ -10,3 +10,5 @@ interface HttpInterface
      * Send a post request to a URL.
      *
      * @param  string $url
+     * @param  array $urlParameters
+    public function __construct(AdapterInterface $adapter)
