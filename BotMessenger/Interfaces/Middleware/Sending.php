7db8447703681c76ae5079bccb34964148318dc1
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-10 11:55:57 +0000

    Cập nhật BotMessenger/Interfaces/Middleware/Sending.php

--- BotMessenger/Interfaces/Middleware/Sending.php
+++ BotMessenger/Interfaces/Middleware/Sending.php
@@ -10,4 +10,5 @@ interface Sending
      * Handle an outgoing message payload before/after it
      * hits the message service.
      *
-     */
+     * @param mixed $payload
+        $this->last_name = $last_name;
