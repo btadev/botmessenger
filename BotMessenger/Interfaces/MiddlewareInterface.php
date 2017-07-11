8b7ec1ad1b825a326a001be1bf2035ff42e8a96a
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-11 08:02:12 +0000

    Cập nhật BotMessenger/Interfaces/MiddlewareInterface.php

--- BotMessenger/Interfaces/MiddlewareInterface.php
+++ BotMessenger/Interfaces/MiddlewareInterface.php
@@ -6,4 +6,4 @@ use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
 use ChienIT\BotMessenger\Interfaces\Middleware\Sending;
 use ChienIT\BotMessenger\Interfaces\Middleware\Captured;
 use ChienIT\BotMessenger\Interfaces\Middleware\Matching;
-        }
+use ChienIT\BotMessenger\Interfaces\Middleware\Received;
