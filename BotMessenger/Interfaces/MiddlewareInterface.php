9d49be6d9673ea688c3482702b703c23724d0414
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-20 11:55:37 +0000

    Cập nhật BotMessenger/Interfaces/MiddlewareInterface.php

--- BotMessenger/Interfaces/MiddlewareInterface.php
+++ BotMessenger/Interfaces/MiddlewareInterface.php
@@ -3,4 +3,5 @@
 namespace ChienIT\BotMessenger\Interfaces;
 
 use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
-    }
+use ChienIT\BotMessenger\Interfaces\Middleware\Sending;
+    /** @var array */
