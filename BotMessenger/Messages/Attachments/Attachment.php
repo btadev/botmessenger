efdff5a18e7415066bb1b942e958a58d386cd443
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-20 09:02:40 +0000

    Cập nhật BotMessenger/Messages/Attachments/Attachment.php

--- BotMessenger/Messages/Attachments/Attachment.php
+++ BotMessenger/Messages/Attachments/Attachment.php
@@ -4,4 +4,5 @@ namespace ChienIT\BotMessenger\Messages\Attachments;
 
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Interfaces\WebAccess;
-     * Save an item in the storage with a specific key and data.
+
+     * @param BotMessenger $bot
