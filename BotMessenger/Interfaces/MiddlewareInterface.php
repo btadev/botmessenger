50f2eccf9bb28841495092d16fb801ff4980455d
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-07-25 22:48:05 +0000

    Cập nhật BotMessenger/Interfaces/MiddlewareInterface.php

--- BotMessenger/Interfaces/MiddlewareInterface.php
+++ BotMessenger/Interfaces/MiddlewareInterface.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Interfaces;
 use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
 use ChienIT\BotMessenger\Interfaces\Middleware\Sending;
 use ChienIT\BotMessenger\Interfaces\Middleware\Captured;
-
+use ChienIT\BotMessenger\Interfaces\Middleware\Matching;
+        }
