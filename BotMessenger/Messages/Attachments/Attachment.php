4bc66ad1779dc70275e307c38a8ca8305fc7de46
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-09 00:07:00 +0000

    Cập nhật BotMessenger/Messages/Attachments/Attachment.php

--- BotMessenger/Messages/Attachments/Attachment.php
+++ BotMessenger/Messages/Attachments/Attachment.php
@@ -33,3 +33,5 @@ abstract class Attachment implements WebAccess
     /**
      * @param string $key
      * @param mixed $value
+     * @return Attachment
+            $this->driver($attributes['driver']);
