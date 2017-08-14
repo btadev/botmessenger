86eabe0732fee9fbf2ecc09357cc0709114e3cf4
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-14 04:43:45 +0000

    Cập nhật BotMessenger/Messages/Attachments/File.php

--- BotMessenger/Messages/Attachments/File.php
+++ BotMessenger/Messages/Attachments/File.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Messages\Attachments;
 class File extends Attachment
 {
     /**
-}
+     * Pattern that messages use to identify file uploads.
+    public function types(IncomingMessage $matchingMessage)
