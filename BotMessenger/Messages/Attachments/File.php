1858f7de400e9f8e480bfe5e624b66ba9e476a91
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-08-15 16:06:00 +0000

    Cập nhật BotMessenger/Messages/Attachments/File.php

--- BotMessenger/Messages/Attachments/File.php
+++ BotMessenger/Messages/Attachments/File.php
@@ -7,4 +7,5 @@ class File extends Attachment
     /**
      * Pattern that messages use to identify file uploads.
      */
-    abstract public function buildPayload(Request $request);
+    const PATTERN = '%%%_FILE_%%%';
+    /**
