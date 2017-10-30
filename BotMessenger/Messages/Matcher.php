90e3c5a3f292ebbd17858eea9f7ef5fe5919fcd7
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-10-30 20:23:00 +0000

    Cập nhật BotMessenger/Messages/Matcher.php

--- BotMessenger/Messages/Matcher.php
+++ BotMessenger/Messages/Matcher.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Messages;
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Commands\Command;
 use ChienIT\BotMessenger\Messages\Incoming\Answer;
-{
+use ChienIT\BotMessenger\Interfaces\DriverInterface;
+
