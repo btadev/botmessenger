aeab5583efcdb2cfb7bea74cb3763fb316d9d472
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-03 15:28:36 +0000

    Cập nhật BotMessenger/Messages/Matcher.php

--- BotMessenger/Messages/Matcher.php
+++ BotMessenger/Messages/Matcher.php
@@ -7,4 +7,5 @@ use ChienIT\BotMessenger\Commands\Command;
 use ChienIT\BotMessenger\Messages\Incoming\Answer;
 use ChienIT\BotMessenger\Interfaces\DriverInterface;
 use ChienIT\BotMessenger\Interfaces\Middleware\Matching;
-namespace ChienIT\BotMessenger\Interfaces;
+use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
+
