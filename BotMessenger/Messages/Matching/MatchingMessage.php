1667289a9ff57f0116e3bd271c07ddf497718fc1
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-03 00:47:43 +0000

    Cập nhật BotMessenger/Messages/Matching/MatchingMessage.php

--- BotMessenger/Messages/Matching/MatchingMessage.php
+++ BotMessenger/Messages/Matching/MatchingMessage.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Messages\Matching;
 use ChienIT\BotMessenger\Commands\Command;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
 
-    /**
+class MatchingMessage
+use Illuminate\Support\Collection;
