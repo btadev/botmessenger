19b56c13977fdfe69fbc6909b94ea1477535be6c
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-11-17 23:55:51 +0000

    Cập nhật BotMessenger/Messages/Outgoing/Actions/Button.php

--- BotMessenger/Messages/Outgoing/Actions/Button.php
+++ BotMessenger/Messages/Outgoing/Actions/Button.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Messages\Outgoing\Actions;
 use JsonSerializable;
 use ChienIT\BotMessenger\Interfaces\QuestionActionInterface;
 
-
+class Button implements JsonSerializable, QuestionActionInterface
+form of a separately written license, or stated as exceptions;
