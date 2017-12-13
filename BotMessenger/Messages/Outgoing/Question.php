c25b2be6c8c168851d08f75ec761bcd0704ac46f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-13 10:29:46 +0000

    Cập nhật BotMessenger/Messages/Outgoing/Question.php

--- BotMessenger/Messages/Outgoing/Question.php
+++ BotMessenger/Messages/Outgoing/Question.php
@@ -7,4 +7,5 @@ use ChienIT\BotMessenger\Interfaces\WebAccess;
 use ChienIT\BotMessenger\Messages\Outgoing\Actions\Button;
 use ChienIT\BotMessenger\Interfaces\QuestionActionInterface;
 
-    {
+class Question implements JsonSerializable, WebAccess
+     * @return array
