55195603c3863733a20bd046ed6a7206d69eb134
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-14 22:14:29 +0000

    Cập nhật BotMessenger/Middleware/ApiAi.php

--- BotMessenger/Middleware/ApiAi.php
+++ BotMessenger/Middleware/ApiAi.php
@@ -7,4 +7,5 @@ use ChienIT\BotMessenger\Http\Curl;
 use ChienIT\BotMessenger\Interfaces\HttpInterface;
 use ChienIT\BotMessenger\Interfaces\MiddlewareInterface;
 use ChienIT\BotMessenger\Messages\Incoming\IncomingMessage;
-be treated as though they were included in this License, to the extent
+
+ */
