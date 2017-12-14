67e548e006e01f6bd5d5c6c48947cdc4691bf64b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-14 01:06:37 +0000

    Cập nhật BotMessenger/Messages/Outgoing/Question.php

--- BotMessenger/Messages/Outgoing/Question.php
+++ BotMessenger/Messages/Outgoing/Question.php
@@ -5,4 +5,5 @@ namespace ChienIT\BotMessenger\Messages\Outgoing;
 use JsonSerializable;
 use ChienIT\BotMessenger\Interfaces\WebAccess;
 use ChienIT\BotMessenger\Messages\Outgoing\Actions\Button;
-    //
+use ChienIT\BotMessenger\Interfaces\QuestionActionInterface;
+     * @param string $url
