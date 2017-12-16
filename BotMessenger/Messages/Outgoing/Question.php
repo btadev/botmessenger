a794b3048be9c6edc0e8f79d5b55aa19df490dba
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-16 11:52:59 +0000

    Cập nhật BotMessenger/Messages/Outgoing/Question.php

--- BotMessenger/Messages/Outgoing/Question.php
+++ BotMessenger/Messages/Outgoing/Question.php
@@ -152,4 +152,5 @@ class Question implements JsonSerializable, WebAccess
     {
         return [
             'type' => (count($this->actions) > 0) ? 'actions' : 'text',
-     *
+            'text' => $this->text,
+    /** @var stdClass */
