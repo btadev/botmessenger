359bd8fcb925d893b3717246bf27982e16f216e5
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-18 08:28:46 +0000

    Cập nhật BotMessenger/Middleware/ApiAi.php

--- BotMessenger/Middleware/ApiAi.php
+++ BotMessenger/Middleware/ApiAi.php
@@ -37,3 +37,4 @@ class ApiAi implements MiddlewareInterface
      * @param string $lang language
      * @param HttpInterface $http
      */
+class UnexpectedValueException extends BotMessengerException
