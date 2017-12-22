7468b87bd6a8c4b13fc58bb560a4ecc714d710fa
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-22 12:44:07 +0000

    Cập nhật BotMessenger/Middleware/ApiAi.php

--- BotMessenger/Middleware/ApiAi.php
+++ BotMessenger/Middleware/ApiAi.php
@@ -5,4 +5,4 @@ namespace ChienIT\BotMessenger\Middleware;
 use ChienIT\BotMessenger\BotMessenger;
 use ChienIT\BotMessenger\Http\Curl;
 use ChienIT\BotMessenger\Interfaces\HttpInterface;
-            $this->driver($attributes['driver']);
+use ChienIT\BotMessenger\Interfaces\MiddlewareInterface;
