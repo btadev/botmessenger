5e5f5dfc34ed4845d7595f27c1cb0b362cbfeb34
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2017-12-23 11:37:17 +0000

    Cập nhật BotMessenger/Messages/Outgoing/Question.php

--- BotMessenger/Messages/Outgoing/Question.php
+++ BotMessenger/Messages/Outgoing/Question.php
@@ -113,4 +113,5 @@ class Question implements JsonSerializable, WebAccess
     /**
      * @return array
      */
-     */
+    public function jsonSerialize()
+            $this->redis->auth($this->auth);
