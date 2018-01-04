5a7184121657999b5b0d8d7699ffddcaf3cd55be
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-04 04:44:18 +0000

    Cập nhật BotMessenger/Middleware/ApiAi.php

--- BotMessenger/Middleware/ApiAi.php
+++ BotMessenger/Middleware/ApiAi.php
@@ -75,4 +75,5 @@ class ApiAi implements MiddlewareInterface
     protected function getResponse(IncomingMessage $message)
     {
         $response = $this->http->post($this->apiUrl, [], [
-namespace ChienIT\BotMessenger\Interfaces;
+            'query' => [$message->getText()],
+
