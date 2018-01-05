a76adda600aecd2d576efd86d56728ab106e08f9
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-05 06:23:50 +0000

    Cập nhật BotMessenger/Middleware/MiddlewareManager.php

--- BotMessenger/Middleware/MiddlewareManager.php
+++ BotMessenger/Middleware/MiddlewareManager.php
@@ -5,3 +5,5 @@ namespace ChienIT\BotMessenger\Middleware;
 use Closure;
 use ChienIT\BotMessenger\BotMessenger;
 use Mpociot\Pipeline\Pipeline;
+use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
+
