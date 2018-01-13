1795b4fdfba1ce3c2bdad8d9d657dba4d481682b
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-13 16:05:07 +0000

    Cập nhật BotMessenger/Middleware/ApiAi.php

--- BotMessenger/Middleware/ApiAi.php
+++ BotMessenger/Middleware/ApiAi.php
@@ -94,4 +94,5 @@ class ApiAi implements MiddlewareInterface
      * @param \ChienIT\BotMessenger\Messages\Incoming\IncomingMessage $message
      * @param BotMessenger $bot
      * @param $next
-    protected $command;
+     *
+     * Return the event payload.
