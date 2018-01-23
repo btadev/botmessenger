b096444a3942c265bb9a9101d744c048e6c0bb49
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-23 11:49:53 +0000

    Cập nhật BotMessenger/Middleware/MiddlewareManager.php

--- BotMessenger/Middleware/MiddlewareManager.php
+++ BotMessenger/Middleware/MiddlewareManager.php
@@ -8,3 +8,5 @@ use Mpociot\Pipeline\Pipeline;
 use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
 use ChienIT\BotMessenger\Interfaces\Middleware\Sending;
 use ChienIT\BotMessenger\Interfaces\Middleware\Captured;
+use ChienIT\BotMessenger\Interfaces\Middleware\Matching;
+
