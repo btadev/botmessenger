aa3d61ed9c045cd972fa01646deb3183fec71ffd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-01-23 03:24:24 +0000

    Cập nhật BotMessenger/Middleware/MiddlewareManager.php

--- BotMessenger/Middleware/MiddlewareManager.php
+++ BotMessenger/Middleware/MiddlewareManager.php
@@ -11,4 +11,5 @@ use ChienIT\BotMessenger\Interfaces\Middleware\Captured;
 use ChienIT\BotMessenger\Interfaces\Middleware\Matching;
 use ChienIT\BotMessenger\Interfaces\Middleware\Received;
 use ChienIT\BotMessenger\Interfaces\MiddlewareInterface;
-}
+
+    public function isConfigured()
