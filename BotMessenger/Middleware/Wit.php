eda07351396785c254286197938e83c21ca2ff64
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-08 03:08:10 +0000

    Cập nhật BotMessenger/Middleware/Wit.php

--- BotMessenger/Middleware/Wit.php
+++ BotMessenger/Middleware/Wit.php
@@ -52,3 +52,5 @@ class Wit implements MiddlewareInterface
 
         $this->response = $this->http->post($endpoint, [], [], [
             'Authorization: Bearer '.$this->token,
+        ]);
+    public function matchesRequest();
