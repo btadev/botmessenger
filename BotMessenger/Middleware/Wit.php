c91c8a9f713eff54f3897a598c0ff703c1ccc4cf
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-11 14:58:30 +0000

    Cập nhật BotMessenger/Middleware/Wit.php

--- BotMessenger/Middleware/Wit.php
+++ BotMessenger/Middleware/Wit.php
@@ -77,3 +77,5 @@ class Wit implements MiddlewareInterface
      * @param \ChienIT\BotMessenger\Messages\Incoming\IncomingMessage $message
      * @param BotMessenger $bot
      * @param $next
+     *
+     */
