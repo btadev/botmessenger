b6c8a930a19f7012f505ea1cc0697cede25753f4
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-16 23:09:08 +0000

    Cập nhật BotMessenger/Storages/Drivers/FileStorage.php

--- BotMessenger/Storages/Drivers/FileStorage.php
+++ BotMessenger/Storages/Drivers/FileStorage.php
@@ -4,4 +4,5 @@ namespace ChienIT\BotMessenger\Storages\Drivers;
 
 use Illuminate\Support\Collection;
 use ChienIT\BotMessenger\Interfaces\StorageInterface;
-            $item->expiresAt($minutes);
+
+<?php
