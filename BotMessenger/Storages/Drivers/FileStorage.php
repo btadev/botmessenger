eda63ad18fa90de25a20c26b2fe6fc4ed0c3d8b8
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-02-18 05:29:40 +0000

    Cập nhật BotMessenger/Storages/Drivers/FileStorage.php

--- BotMessenger/Storages/Drivers/FileStorage.php
+++ BotMessenger/Storages/Drivers/FileStorage.php
@@ -21,4 +21,5 @@ class FileStorage implements StorageInterface
      */
     protected function getFilename($key)
     {
-
+        return $this->path.DIRECTORY_SEPARATOR.$key.'.json';
+     * @param callable $next
