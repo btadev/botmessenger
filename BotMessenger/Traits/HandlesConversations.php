dd08c30381c3c54e0ea0c6e7e9705072b9163d89
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-08 10:32:31 +0000

    Cập nhật BotMessenger/Traits/HandlesConversations.php

--- BotMessenger/Traits/HandlesConversations.php
+++ BotMessenger/Traits/HandlesConversations.php
@@ -4,4 +4,5 @@ namespace ChienIT\BotMessenger\Traits;
 
 use Closure;
 use Illuminate\Support\Collection;
-    /**
+use Opis\Closure\SerializableClosure;
+namespace ChienIT\BotMessenger\Interfaces\Middleware;
