1de12461a9f57b28d5cab7d0c68fe090ea3cbd42
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-09 02:17:11 +0000

    Cập nhật BotMessenger/Storages/Drivers/RedisStorage.php

--- BotMessenger/Storages/Drivers/RedisStorage.php
+++ BotMessenger/Storages/Drivers/RedisStorage.php
@@ -103,4 +103,5 @@ class RedisStorage implements StorageInterface
         $this->redis = new Redis();
         $this->redis->connect($this->host, $this->port);
         if ($this->auth !== null) {
-use ChienIT\BotMessenger\Interfaces\Middleware\Heard;
+            $this->redis->auth($this->auth);
+}
