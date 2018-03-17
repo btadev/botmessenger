dd91cd5be688e6b6b532b2fca53ceb31464a3da3
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-17 12:53:45 +0000

    Cập nhật BotMessenger/Traits/HandlesConversations.php

--- BotMessenger/Traits/HandlesConversations.php
+++ BotMessenger/Traits/HandlesConversations.php
@@ -105,4 +105,5 @@ trait HandlesConversations
     {
         /*
          * Only remove it from the cache if it was not modified
-{
+         * after we loaded the data from the cache.
+     * @return $this
