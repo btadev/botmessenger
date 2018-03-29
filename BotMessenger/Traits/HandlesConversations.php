05fddb9f62745c9c353614c36aec5b9d571b380f
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-03-29 17:41:27 +0000

    Cập nhật BotMessenger/Traits/HandlesConversations.php

--- BotMessenger/Traits/HandlesConversations.php
+++ BotMessenger/Traits/HandlesConversations.php
@@ -6,3 +6,5 @@ use Closure;
 use Illuminate\Support\Collection;
 use Opis\Closure\SerializableClosure;
 use ChienIT\BotMessenger\Drivers\DriverManager;
+use ChienIT\BotMessenger\Interfaces\ShouldQueue;
+class InlineConversation extends Conversation
