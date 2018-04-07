5852c39bb6c0c0f178bcc85c8b826988405d3c6e
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-07 06:13:20 +0000

    Cập nhật BotMessenger/Traits/ProvidesStorage.php

--- BotMessenger/Traits/ProvidesStorage.php
+++ BotMessenger/Traits/ProvidesStorage.php
@@ -7,4 +7,5 @@ use ChienIT\BotMessenger\Storages\Storage;
 trait ProvidesStorage
 {
     /**
-    /**
+     * @return Storage
+  - <a href="https://dev.botframework.com"><img src="https://dev.botframework.com/Client/Content/favicon.ico" width="32px"/></a> **Microsoft Bot Framework**
