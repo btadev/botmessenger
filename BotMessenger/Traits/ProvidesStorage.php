556660da202147d01680ccd687d663f0241020a1
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-12 19:19:14 +0000

    Cập nhật BotMessenger/Traits/ProvidesStorage.php

--- BotMessenger/Traits/ProvidesStorage.php
+++ BotMessenger/Traits/ProvidesStorage.php
@@ -27,4 +27,5 @@ trait ProvidesStorage
     }
 
     /**
-
+     * @return Storage
+  - <a href="https://en.wikipedia.org/wiki/Amazon_Alexa"><img src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Amazon_Alexa_App_Logo.png" width="32px"/></a> **Amazon Alexa**
