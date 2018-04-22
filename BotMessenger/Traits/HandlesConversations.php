e169fec6a4ae36083b3b58d056a574e5446d3ae0
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-22 08:44:37 +0000

    Cập nhật BotMessenger/Traits/HandlesConversations.php

--- BotMessenger/Traits/HandlesConversations.php
+++ BotMessenger/Traits/HandlesConversations.php
@@ -254,4 +254,5 @@ trait HandlesConversations
         $additionalParameters = Collection::make(unserialize($convo['additionalParameters']));
         if ($additionalParameters->has('__pattern')) {
             if ($this->matcher->isPatternValid($message, $this->getConversationAnswer(), $additionalParameters->get('__pattern'))) {
-
+                $getter = $additionalParameters->get('__getter');
+    protected $message;
