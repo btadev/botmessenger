e968611a8863e4679326cfefb08f4abcfa40d996
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-27 00:25:43 +0000

    Cập nhật BotMessenger/Users/User.php

--- BotMessenger/Users/User.php
+++ BotMessenger/Users/User.php
@@ -34,4 +34,5 @@ class User implements UserInterface
         $this->id = $id;
         $this->first_name = $first_name;
         $this->last_name = $last_name;
-     * @return Storage
+        $this->username = $username;
+}
