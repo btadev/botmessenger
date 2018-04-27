22e91a04abdd8b1080e4c6315e84cfb5308fc8fd
Nguyễn Đức Chiến <nguyenchienbg2k@gmail.com>
2018-04-27 18:42:50 +0000

    Cập nhật BotMessenger/Users/User.php

--- BotMessenger/Users/User.php
+++ BotMessenger/Users/User.php
@@ -12,4 +12,4 @@ class User implements UserInterface
     /** @var string */
     protected $first_name;
 
-  - <a href="https://hangouts.google.com"><img src="https://ssl.gstatic.com/chat/startpage/favicon_f1bac5c7ba3154b58080de921eb6d5ea.ico" width="32px"/></a> **Hangouts Chat**
+    /** @var string */
